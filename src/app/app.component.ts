cda80bd7ec3cbfba236f321d3308667848cd4761
Jhon Omar <jhonomar109@gmail.com>
2023-12-13 19:18:23 -0500

    Primer commit: Descripción del commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FacultadComponent } from './components/facultad/facultad.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FacultadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'myPrimerProyecto';
+  university: string = "Universidad de Nariño"
 }
---
+++ src/app/components/facultad/facultad.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-facultad',
+  standalone: true,
+  imports: [],
+  templateUrl: './facultad.component.html',
+  styleUrl: './facultad.component.css'
+})
+export class FacultadComponent {
+  name: string = "Ingenieria"
+
+}
